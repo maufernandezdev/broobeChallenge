d7a21fb1a2ec5b053323a94505648affad91b741
Mauricio Fernandez <mfernandezuk@hotmail.com>
2023-02-05 13:16:36 -0300

    styles for home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <div className='container'>
       <h1>Broobe challenge</h1>
+      <div className='container__description'>
+        <p>You can log in with "Get started" or also if you want you can see the documentation on Github.</p>
+      </div>
+      <div className='buttonsContainer'>
+        <Link to='/login'>Get started</Link>
+        <Link to='https://github.com/maufernandezdev/broobeChallenge' target='_blank'>Docs</Link>
+      </div>
     </div>
   )
 }
